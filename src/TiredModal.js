481188efd0d4a6f7a6e3d9f289891354cbe45380
zee2013234 <zee2013234@kaist.ac.kr>
2020-06-04 11:33:18 +0900

    fm;l

--- src/TiredModal.js
+++ src/TiredModal.js
@@ -18,6 +18,30 @@ class TiredModal extends React.Component {
     render() {
         return (
             <Modal show={this.props.show}>
+                        <div >
+                    <h5 className="modal-title">Oats with nuts and berries</h5>
+                </div>
+                <div>
+                            <img src="img/breakfast.jpg" className="w-100 image-border" alt="..." />
+                </div>
+                <div className="modal-body recipe">
+                            <h5>Ingredients:</h5>
+                            <p>Oats, Berries(blueberries/strawberries), Almond milk, Nuts</p>
+                            <h5>Nutrients:</h5>
+                    <p>Protein: 10g, Carbohydrates: 12g, Fats: 5g</p>
+                    </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-primary" onClick={() => this.props.changeModal(this.props.modalType)}>Confirm</button>
+                            <button className="btnSuggestion" onClick={this.changeContent}>Give me another suggestion!</button>
+                            <button className="btn btn-secondary" onClick={() => this.props.changeModal(this.props.modalType)}>Close</button>
+                        </div>
+            </Modal>
+        )
+    }
+
+    /*render() {
+        return (
+            <Modal show={this.props.show}>
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -30,16 +54,16 @@ class TiredModal extends React.Component {
                         <div className="modal-body recipe">
                             <h5>You will need:</h5>
                             <p>Oats: 30g(Protein: 1g Carbohydrates: 12g Fats: 1g)
-                                <br/>Berries(blueberries/strawberries): 100g(Protein: 1g Carbohydrates: 12g Fats: 1g)
-                                <br/>Almond milk: 100(Protein: 1g Carbohydrates: 12g Fats: 1g)g
-                                <br/>Nuts: 20g(Protein: 1g Carbohydrates:
+                                <br />Berries(blueberries/strawberries): 100g(Protein: 1g Carbohydrates: 12g Fats: 1g)
+                                <br />Almond milk: 100(Protein: 1g Carbohydrates: 12g Fats: 1g)g
+                                <br />Nuts: 20g(Protein: 1g Carbohydrates:
                                 12g Fats: 1g)</p>
                             <h5>Recipe:</h5>
                             <p>Leave the oats soak in the milk in the fridge overnight or cook the oats in milk for 5-10 minutes. Put the berries and nuts on top and enjoy.</p>
                             <h5>Total nutrients:</h5>
                             <p>Protein: 10g
-                                <br/>Carbohydrates: 12g
-                                <br/>Fats: 5g</p>
+                                <br />Carbohydrates: 12g
+                                <br />Fats: 5g</p>
                         </div>
                         <div className="modal-footer">
                             <button className="btn btn-primary" onClick={() => this.props.changeModal(this.props.modalType)}>Confirm</button>
@@ -50,7 +74,7 @@ class TiredModal extends React.Component {
                 </div>
             </Modal>
         )
-    }
+    }*/
 }
 
 export default TiredModal;
