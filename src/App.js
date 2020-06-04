f3309478ae6f3f2003ddffc79486375ebc5cdfee
Ern Chern <39647817+ernchern@users.noreply.github.com>
2020-06-04 19:09:36 +0800

    button

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      currPage: "main",
+      currPage: "login",
     };
 
     this.setPage = this.setPage.bind(this);
