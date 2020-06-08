2cff9fb11d7420d5c4881353e14c68c9d1b97f70
Lala Asgarova <askerova.lala.05@gmail.com>
2020-06-08 11:58:48 +0900

    Updates

--- precache-manifest.35b4c40982a8dcdd58e5ffb1faa14d7b.js
+++ precache-manifest.35b4c40982a8dcdd58e5ffb1faa14d7b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0df50d68acae436f46901a85ead068f5",
+    "revision": "3196bdeb17b64c378ca8dab93030aa34",
     "url": "/superfood/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/superfood/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "e04f629f63db90868c73",
-    "url": "/superfood/static/css/main.9481208a.chunk.css"
+    "revision": "ec64bc5aa9f90d3c5fb6",
+    "url": "/superfood/static/css/main.64dfcf14.chunk.css"
   },
   {
     "revision": "e20711bf24e3fdb63119",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/superfood/static/js/2.2a138b5f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e04f629f63db90868c73",
-    "url": "/superfood/static/js/main.dae74f6a.chunk.js"
+    "revision": "ec64bc5aa9f90d3c5fb6",
+    "url": "/superfood/static/js/main.dca091ce.chunk.js"
   },
   {
     "revision": "f4b3ba99915064f715da",
