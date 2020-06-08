18909c93d733c51a290e1b90bd4ac2a662d3b5d0
gcamilo <gcamilo7@gmail.com>
2020-06-08 14:36:51 +0900

    pw shows when editing

--- src/PersonalRegister.js
+++ src/PersonalRegister.js
@@ -58,7 +58,9 @@ class PersonalRegister extends React.Component {
                                     </Col>
                                     <Col lg="6">
                                         <label>Password<span style={{color:'red'}}>*</span></label>
-                                        <input type="password" name="password" autoComplete='off' id="password" onChange={(event) => this.props.setResponse("pw", event.target.value)}/>
+                                        <input
+                                        value={this.props.responseData.pw}
+                                        type="password" name="password" autoComplete='off' id="password" onChange={(event) => this.props.setResponse("pw", event.target.value)}/>
                                     </Col>
                                     <Col lg="12" className="country-col">
                                         <label>Country of Residence<span style={{color:'red'}}>*</span></label>
